c719746e9e58349d553980b840fd8d04d8e9062b
cclabdis <cclabdis@gmail.com>
2023-07-31 11:22:26 -0300

    teste para o hub

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -12,36 +12,16 @@ export async function getGames(req, res) {
 
 export async function createGame (req, res) {
     const { name, image, stockTotal, pricePerDay } = req.body
-
-    // pricePerDay = pricePerDay * 100
-    // stockTotal = parseInt(stockTotal)
     
         try {
-            // const { rows: game } = await connection.query(`
-            //     SELECT * FROM games WHERE name = ( $1 )
-            // `, [name])
-
-            // if (stockTotal <= 0 || pricePerDay <= 0) {
-            // res.sendStatus(400)
-            // }
-            // if (game.length > 0) return res.sendStatus(409)
-    
-            // await connection.query(`
-            //     INSERT INTO games ( name, image, "stockTotal", "pricePerDay" ) VALUES ( $1, $2, $3, $4,)
-            // `, [name, image, stockTotal, pricePerDay])
-    
-            // res.sendStatus(201)
-
+        if (!name) {res.sendStatus(400)}
+        if ( stockTotal <= 0 || pricePerDay <= 0) {res.sendStatus(400) }
       
-        if (!name || stockTotal <= 0 || pricePerDay <= 0) {res.sendStatus(400) }
-      
-          // Verificar se o nome do jogo já existe na tabela
           const existingGame = await db.query('SELECT 1 FROM games WHERE name = $1', [name]);
           if (existingGame.rows.length > 0) {
             res.sendStatus(409)
           }
       
-          // Inserir o novo jogo na tabela
           const queryText = 'INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4)';
           await db.query(queryText, [name, image, stockTotal, pricePerDay]);
       
