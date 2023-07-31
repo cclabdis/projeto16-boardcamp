665bcbb382171660fd283bc79d02f351ae5208a9
cclabdis <cclabdis@gmail.com>
2023-07-31 15:23:44 -0300

    ajuste schema name game

--- src/schemas/game.schema.js
+++ src/schemas/game.schema.js
@@ -1,7 +1,7 @@
 import joi from "joi"
 
 export const gameSchema = joi.object({
-    name: joi.string().allow("").required(),
+    name: joi.string().required(),
     image: joi.string().trim().uri().required(),
     stockTotal: joi.number().positive().integer().required(),
     pricePerDay: joi.number().integer().required()
