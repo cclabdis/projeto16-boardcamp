4835dcdd7640b6d6ef8fd921434116532a4af738
cclabdis <cclabdis@gmail.com>
2023-07-31 16:24:00 -0300

    ajuste schema costuemr

--- src/schemas/customers.schema.js
+++ src/schemas/customers.schema.js
@@ -1,7 +1,7 @@
 import joi from "joi"
 
 export const customerSchema = joi.object({
-    name: joi.string().allow("").required(),
+    name: joi.string().required(),
     phone: joi.number().required(),
     cpf: joi.string().regex(/^\d{11}$/).required(),
     birthday: joi.date().required()
