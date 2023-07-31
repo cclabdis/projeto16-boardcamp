5f1e0f8a608bbb526ab7ef075f3e20db9452c037
cclabdis <cclabdis@gmail.com>
2023-07-31 11:40:25 -0300

    teste para o hub

--- src/schemas/game.schema.js
+++ src/schemas/game.schema.js
@@ -1,7 +1,7 @@
 import joi from "joi"
 
 export const gameSchema = joi.object({
-    name: joi.string(),
+    name: joi.string().allow(""),
     image: joi.string().trim().uri().required(),
     stockTotal: joi.number().positive().integer().required(),
     pricePerDay: joi.number().positive().integer().required()
