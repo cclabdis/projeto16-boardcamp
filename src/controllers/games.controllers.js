d240abbaa20372e0bff9ad5408fe9f1cc67d2bd1
cclabdis <cclabdis@gmail.com>
2023-07-31 09:57:05 -0300

    teste para o hub

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -12,25 +12,42 @@ export async function getGames(req, res) {
 
 export async function createGame () {
     const { name, image, stockTotal, pricePerDay } = req.body
-    try{
 
-        
+    pricePerDay = pricePerDay * 100
+    stockTotal = parseInt(stockTotal)
+    
+        try {
+            const { rows: game } = await connection.query(`
+                SELECT * FROM games WHERE name = ( $1 )
+            `, [name])
 
-        if (stockTotal <= 0 || pricePerDay <= 0) {
+            if (stockTotal <= 0 || pricePerDay <= 0) {
             res.sendStatus(400)
-          }
+            }
+            if (game.length > 0) return res.sendStatus(409)
+    
+            await connection.query(`
+                INSERT INTO games ( name, image, "stockTotal", "pricePerDay" ) VALUES ( $1, $2, $3, $4,)
+            `, [name, image, stockTotal, pricePerDay])
+    
+            res.sendStatus(201)
+
+
+        // if (stockTotal <= 0 || pricePerDay <= 0) {
+        //     res.sendStatus(400)
+        //   }
       
-          // Verificar se o nome do jogo já existe na tabela
-          const existingGame = await db.query('SELECT 1 FROM games WHERE name = $1', [name]);
-          if (existingGame.rows.length > 0) {
-            res.sendStatus(409)
-          }
+        //   // Verificar se o nome do jogo já existe na tabela
+        //   const existingGame = await db.query('SELECT 1 FROM games WHERE name = $1', [name]);
+        //   if (existingGame.rows.length > 0) {
+        //     res.sendStatus(409)
+        //   }
       
-          // Inserir o novo jogo na tabela
-          const queryText = 'INSERT INTO games (name, image, stockTotal, pricePerDay) VALUES ($1, $2, $3, $4)';
-          await db.query(queryText, [name, image, stockTotal, pricePerDay]);
+        //   // Inserir o novo jogo na tabela
+        //   const queryText = 'INSERT INTO games (name, image, stockTotal, pricePerDay) VALUES ($1, $2, $3, $4)';
+        //   await db.query(queryText, [name, image, stockTotal, pricePerDay]);
       
-          res.sendStatus(201)
+        //   res.sendStatus(201)
      
 
     }catch(err) {
--- src/schemas/game.schema.js
+++ src/schemas/game.schema.js
@@ -1,15 +1,10 @@
-// id: 1,
-//                 name: 'Banco Imobiliário',
-//                 image: 'http://',
-//                 stockTotal: 3,
-//                 pricePerDay: 1500
 
 import joi from "joi"
 
 export const gameSchema = joi.object({
     name: joi.string().required(),
-    image: joi.string(),
-    stockTotal: joi.number().required(),
-    pricePerDay: joi.number().required()
+    image: joi.string().trim().uri().required(),
+    stockTotal: joi.number().positive().integer().required(),
+    pricePerDay: joi.number().positive().integer().required()
 
 })
