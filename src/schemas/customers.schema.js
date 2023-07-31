8c06f59f9034a1ec7676b3d3030ac0bba6dafb97
cclabdis <cclabdis@gmail.com>
2023-07-31 15:15:05 -0300

    ajuste customers

--- src/schemas/customers.schema.js
+++ src/schemas/customers.schema.js
@@ -1,19 +1,10 @@
 import joi from "joi"
 
 export const customerSchema = joi.object({
-    name: joi.string().allow(""),
+    name: joi.string().allow("").required(),
     phone: joi.number().required(),
     cpf: joi.string().regex(/^\d{11}$/).required(),
     birthday: joi.date().required()
 
 })
 
-// ```jsx
-// {
-//   id: 1, 
-//   name: 'João Alfredo',
-//   phone: '21998899222',
-//   cpf: '01234567890',
-//   birthday: '1992-10-25'
-// }
-// ```
