aba19dd583955cbae8d274d8dc494a233350caae
cclabdis <cclabdis@gmail.com>
2023-07-31 15:16:50 -0300

    ajuste no schema para priceperday

--- src/schemas/game.schema.js
+++ src/schemas/game.schema.js
@@ -1,9 +1,9 @@
 import joi from "joi"
 
 export const gameSchema = joi.object({
-    name: joi.string().allow(""),
+    name: joi.string().allow("").required(),
     image: joi.string().trim().uri().required(),
     stockTotal: joi.number().positive().integer().required(),
-    pricePerDay: joi.number().positive().integer().required()
+    pricePerDay: joi.number().integer().required()
 
 })
